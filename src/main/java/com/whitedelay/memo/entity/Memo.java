a821b7f1247963c4e3e50881844e040b1c429fe2
delay-100 <qor3127@naver.com>
2024-05-15 20:24:44 +0900

    chapter3. Entity 및 yml 수정

--- src/main/java/com/whitedelay/memo/entity/Memo.java
+++ src/main/java/com/whitedelay/memo/entity/Memo.java
@@ -1,5 +1,6 @@
 package com.whitedelay.memo.entity;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -10,6 +11,7 @@ import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@Entity
 public class Memo {
 
     @Id
